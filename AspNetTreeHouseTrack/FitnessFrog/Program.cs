b3f2d62e3bd6d5ab6fe71bc2d6a8e557efcc1073
Thomas Vest Nielsen <nielsen192@gmail.com>
2016-09-25 12:53:16 +0200

    Started the course (and skipped a bit)

---
+++ AspNetTreeHouseTrack/FitnessFrog/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessFrog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            int runningTotal = 0;
+            
+            // Repeat until the user quits
+            while (quit != true)
+            {
+                
+                
+                // Prompt the user for minutes exercised
+                Console.Write("Please enter the minutes exercised: ");
+                string entry = Console.ReadLine();
+                if (entry != "quit")
+                {
+                    int minutes = int.Parse(entry);
+                    // Add minutes exercised to total
+                    runningTotal = runningTotal + minutes;
+                }
+                else
+                {
+                    break;
+                }
+                
+
+                
+
+                // Display total minutes exercised to the screen
+                Console.WriteLine("You've exercised " + runningTotal + " minutes");
+            }
+            
+
+            
+
+
+        }
+    }
+}
---
+++ AspNetTreeHouseTrack/TreehouseDefense/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Map map = new Map(8,5);
+            
+            Point point = new Point(4,2);
+
+            Console.WriteLine(point.DistanceTo(5,5));
+
+            
+        }
+    }
+}
